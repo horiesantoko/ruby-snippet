876e243f35ae3333b127eb020a0cb31ba330bdb1
hoiesantoko <hoiesantoko@gmail.com>
2012-05-24 10:48:55 +0900

    add todo.rb

---
+++ todo.rb
@@ -0,0 +1,18 @@
+def egrep(pattern)
+  Dir['**/*.rb'].each do |fn|
+    count = 0
+    open(fn) do |f|
+      while line = f.gets
+        count += 1
+        if line =~ pattern
+          puts "#{fn}:#{count}:#{line}"
+        end
+      end
+    end
+  end
+end
+
+desc "Look for TODO and FIXME tags in the code"
+task :todo do
+  egrep /(FIXME|TODO|TBD)/
+end
